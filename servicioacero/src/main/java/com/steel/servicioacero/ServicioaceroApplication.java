37085121bdc9f4694caabc5d18f5ad6fbd49ee7b
PaolaMorales19 <ap.morales.anaya@gmail.com>
2024-09-21 17:18:54 -0600

    .

---
+++ servicioacero/src/main/java/com/steel/servicioacero/ServicioaceroApplication.java
@@ -0,0 +1,13 @@
+package com.steel.servicioacero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioaceroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioaceroApplication.class, args);
+	}
+
+}
---
+++ servicioacero/src/test/java/com/steel/servicioacero/ServicioaceroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.steel.servicioacero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioaceroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
