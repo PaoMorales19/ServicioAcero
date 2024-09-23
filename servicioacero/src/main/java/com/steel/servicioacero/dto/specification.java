a3bc5c0fecf69c3cccc157606ae06457a360b236
PaolaMorales19 <ap.morales.anaya@gmail.com>
2024-09-22 20:31:42 -0600

    SPECIFICATION

--- servicioacero/src/main/java/com/steel/servicioacero/dto/specification.java
+++ servicioacero/src/main/java/com/steel/servicioacero/dto/specification.java
@@ -1,5 +1,26 @@
 package com.steel.servicioacero.dto;
 
 public class specification {
+    private String quality;
+    private Integer quantity;
+
+    public specification(String quality, int quantity) {
+        this.quality = quality;
+        this.quantity = quantity;
+    }
+
+    public String getQuality() {
+        return quality;
+    }
+    public void setQuality(String quality) {
+        this.quality = quality;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
     
 }
