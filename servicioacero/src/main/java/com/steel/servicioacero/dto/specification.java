7d6d3e6b6172f6cfe1cb98baa076f69915219332
davidmoralm <david.moralm02@gmail.com>
2024-10-11 17:47:01 -0600

    Add amount property to specification dto class

--- servicioacero/src/main/java/com/steel/servicioacero/dto/specification.java
+++ servicioacero/src/main/java/com/steel/servicioacero/dto/specification.java
@@ -3,24 +3,36 @@ package com.steel.servicioacero.dto;
 public class specification {
     private String quality;
     private Integer quantity;
+    private Double amount;
 
-    public specification(String quality, int quantity) {
+    public specification(String quality, Integer quantity, Double amount) {
         this.quality = quality;
         this.quantity = quantity;
+        this.amount = amount;
     }
 
     public String getQuality() {
         return quality;
     }
+
     public void setQuality(String quality) {
         this.quality = quality;
     }
-    public int getQuantity() {
+
+    public Integer getQuantity() {
         return quantity;
     }
-    public void setQuantity(int quantity) {
+
+    public void setQuantity(Integer quantity) {
         this.quantity = quantity;
     }
 
-    
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
 }
