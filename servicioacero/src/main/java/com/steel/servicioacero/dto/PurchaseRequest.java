9fa936de08d07800510ef6eb6590712ed699b6a7
davidmoralm <david.moralm02@gmail.com>
2024-10-13 01:11:41 -0600

    Add OrderType property and nested enum declaration to PurchaseRequest dto class

--- servicioacero/src/main/java/com/steel/servicioacero/dto/PurchaseRequest.java
+++ servicioacero/src/main/java/com/steel/servicioacero/dto/PurchaseRequest.java
@@ -4,19 +4,27 @@ public class PurchaseRequest {
     private String provider;
     private specification specifications;
     private String deliveryTime;
+    private OrderType orderType;
 
-    public PurchaseRequest(String provider, specification specifications, String deliveryTime) {
+    public enum OrderType {
+        Normal, Urgent
+    }
+
+    public PurchaseRequest(String provider, specification specifications, String deliveryTime, OrderType orderType) {
         this.provider = provider;
         this.specifications = specifications;
         this.deliveryTime = deliveryTime;
+        this.orderType = orderType;
     }
 
     public String getProvider() {
         return provider;
     }
+
     public void setProvider(String provider) {
         this.provider = provider;
     }
+
     public specification getSpecifications() {
         return specifications;
     }
@@ -31,6 +39,14 @@ public class PurchaseRequest {
 
     public void setDeliveryTime(String deliveryTime) {
         this.deliveryTime = deliveryTime;
+    }
+
+    public OrderType getOrderType() {
+        return orderType;
+    }
+
+    public void setOrderType(OrderType orderType) {
+        this.orderType = orderType;
     }
 
 }
