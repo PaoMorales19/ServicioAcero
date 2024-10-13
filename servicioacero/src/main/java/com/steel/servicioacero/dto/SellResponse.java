966a43be7e91f1561f1bb11ded366255aab5fe0e
davidmoralm <david.moralm02@gmail.com>
2024-10-13 01:31:46 -0600

    Refactor SellResponse dto class

--- servicioacero/src/main/java/com/steel/servicioacero/dto/SellResponse.java
+++ servicioacero/src/main/java/com/steel/servicioacero/dto/SellResponse.java
@@ -3,25 +3,29 @@ package com.steel.servicioacero.dto;
 public class SellResponse {
     private String Id;
     private String message;
+
     public SellResponse() {
     }
+
     public SellResponse(String id, String message) {
         Id = id;
         this.message = message;
     }
+
     public String getId() {
         return Id;
     }
+
     public void setId(String id) {
         Id = id;
     }
+
     public String getMessage() {
         return message;
     }
+
     public void setMessage(String message) {
         this.message = message;
     }
 
-    
-    
 }
