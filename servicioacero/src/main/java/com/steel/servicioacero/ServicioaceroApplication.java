33cf5d5faf10b3c0b19b381453620669422fdf7e
davidmoralm <david.moralm02@gmail.com>
2024-10-15 03:47:48 -0600

    Refactor class name to follow camel case convention

--- servicioacero/src/main/java/com/steel/servicioacero/ServicioaceroApplication.java
+++ servicioacero/src/main/java/com/steel/servicioacero/ServicioaceroApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ServicioaceroApplication {
+public class ServicioAceroApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServicioaceroApplication.class, args);
+		SpringApplication.run(ServicioAceroApplication.class, args);
 	}
 
 }
