4b2ba1e7af002f5fe1d58f9465b2bc5aaa7f4f91
davidmoralm <david.moralm02@gmail.com>
2024-10-15 03:09:41 -0600

    Modified JmsConsumer_Ap to deserialize JSON content string message into PurchaseRequest instance and getting properties from it

--- jmsaprobar/src/main/java/com/soa/jmsaprobar/JmsConsumer_Ap.java
+++ jmsaprobar/src/main/java/com/soa/jmsaprobar/JmsConsumer_Ap.java
@@ -3,11 +3,36 @@ package com.soa.jmsaprobar;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import com.steel.servicioacero.dto.PurchaseRequest;
+
 @Component
 public class JmsConsumer_Ap {
-    @JmsListener(destination = "compras.in")
+
+  @JmsListener(destination = "compras.in")
   public void receiveMessage(String message) {
-    System.out.println("Mensaje desencolado: " + message);
+
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    try {
+
+      // Deserialize the JSON content string to a PurchaseRequest object
+      PurchaseRequest purchaseRequest = objectMapper.readValue(message, PurchaseRequest.class);
+      System.out.println("\nMensaje desencolado: " + message);
+
+      // Getting specific properties from the deserialized JSON as a PurchaseRequest instance
+      System.out.println("Monto: " + purchaseRequest.getSpecification().getAmount());
+      System.out.println("Tipo de orden: " + purchaseRequest.getOrderType());
+
+    } catch (JsonProcessingException e) {
+
+      e.printStackTrace();
+      System.err.println("Failed to parse message: " + message);
+
+    }
+
   }
 
 }
