0ce54bee022df481f49971e3da26e2a77264cfa3
David Morales Almeida <98621575+DavidMorAlm@users.noreply.github.com>
2024-10-23 20:09:44 -0600

    Update and rename JmsmgmtApplication.java to JmsMgmtApplication.java

--- jmsmgmt/src/main/java/com/soa/jmsmgmt/JmsMgmtApplication.java
+++ jmsmgmt/src/main/java/com/soa/jmsmgmt/JmsMgmtApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JmsmgmtApplication {
+public class JmsMgmtApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JmsmgmtApplication.class, args);
