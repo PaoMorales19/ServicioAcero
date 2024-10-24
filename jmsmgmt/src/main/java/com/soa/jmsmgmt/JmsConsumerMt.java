cf0e5ce5a1b1f6445baaaa18ed30abb8a1c868e9
davidmoralm <david.moralm02@gmail.com>
2024-10-23 20:07:21 -0600

    Refactor JmsConsumerMt to implement MessageListener interface
    This commit refactors the JmsConsumerMt class to implement the MessageListener interface. It also adds the necessary dependencies and autowires the JmsProducerMt class. The onMessage method is overridden to handle incoming messages. The commit removes unused imports and commented code.

--- jmsmgmt/src/main/java/com/soa/jmsmgmt/JmsConsumerMt.java
+++ jmsmgmt/src/main/java/com/soa/jmsmgmt/JmsConsumerMt.java
@@ -1,19 +1,84 @@
 package com.soa.jmsmgmt;
 
-import org.springframework.jms.annotation.JmsListener;
-import org.springframework.stereotype.Component;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+import javax.jms.JMSException;
 
+// import com.fasterxml.jackson.databind.ObjectMapper;
+// import com.fasterxml.jackson.core.JsonProcessingException;
+
+// import com.steel.servicioacero.dto.PurchaseRequest;
+// import com.steel.servicioacero.dto.OrderType;
+
+import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * 
  */
 @Component
-public class JmsConsumerMt {
-  
+public class JmsConsumerMt implements MessageListener {
 
+  @Autowired
+  private JmsProducerMt jmsProducer;
 
-  @JmsListener(destination = "gerencia.in")
-  public void receiveMessage(String message) {
-    System.out.println("Mensaje desencolado: " + message);
+  @Override
+  public void onMessage(Message message) {
+
+    // ObjectMapper objectMapper = new ObjectMapper();
+    // PurchaseRequest purchaseRequest = null;
+
+    try {
+
+      if (message instanceof TextMessage) {
+
+        String text = ((TextMessage) message).getText();
+
+        System.out.println("\nRECEIVED MESSAGE from [amq.gerencia.in]-----\n");
+
+        System.out.println("HEADERS-----\nDeliveryMode: " + message.getJMSDeliveryMode());
+        System.out.println("Priority: " + message.getJMSPriority());
+        System.out.println("TimeToLive: " + message.getJMSExpiration());
+        // System.out.println("Correlation ID: " + message.getJMSCorrelationID());
+        System.out.println("Type: " + message.getJMSType());
+
+        // System.out.println("\nPROPERTIES-----\nmyProperty: " +
+        // message.getStringProperty("myProperty"));
+
+        System.out.println("\nBODY-----\nBodyJSON: " + text + "\n");
+
+        /* 
+        try {
+
+          // Deserialize the JSON content string to a PurchaseRequest object
+          purchaseRequest = objectMapper.readValue(text, PurchaseRequest.class);
+
+        } catch (JsonProcessingException e) {
+
+          e.printStackTrace();
+          System.err.println("Failed to parse message: ");
+
+        }
+        */
+
+        // Getting specific properties from the deserialized JSON as a PurchaseRequest instance
+        // Double amount = purchaseRequest.getSpecification().getAmount();
+        // OrderType orderType = purchaseRequest.getOrderType();
+        
+        // Send the aproved message to the [amq.compras.out] queue
+        jmsProducer.sendMessage("amq.compras.out", "{\"message\":\"La solicitud de compra fue aprobada por la gerencia.\"}", 9, 60000L);
+        System.out.println("\nLa solicitud de compra fue aprobada por la gerencia.");
+
+      }
+
+    } catch (JMSException e) {
+
+      e.printStackTrace();
+      System.err.println(e);
+
+    }
+
   }
+
 }
