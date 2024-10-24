2e244a822fd3c93fcfccfaf2bf293faadd44a2f7
davidmoralm <david.moralm02@gmail.com>
2024-10-23 20:11:34 -0600

    Refactor JmsMgmtApplication class to fix typo in class name

--- jmsmgmt/src/main/java/com/soa/jmsmgmt/JmsMgmtApplication.java
+++ jmsmgmt/src/main/java/com/soa/jmsmgmt/JmsMgmtApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JmsMgmtApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JmsmgmtApplication.class, args);
+		SpringApplication.run(JmsMgmtApplication.class, args);
 	}
 
 }
