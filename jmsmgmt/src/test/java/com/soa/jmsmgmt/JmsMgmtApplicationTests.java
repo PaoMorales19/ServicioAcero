12f1b76d63c0309cd969e81470ac02aec4a9a011
David Morales Almeida <98621575+DavidMorAlm@users.noreply.github.com>
2024-10-23 20:10:17 -0600

    Update and rename JmsmgmtApplicationTests.java to JmsMgmtApplicationTests.java

--- jmsmgmt/src/test/java/com/soa/jmsmgmt/JmsMgmtApplicationTests.java
+++ jmsmgmt/src/test/java/com/soa/jmsmgmt/JmsMgmtApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class JmsmgmtApplicationTests {
+class JmsMgmtApplicationTests {
 
 	@Test
 	void contextLoads() {
